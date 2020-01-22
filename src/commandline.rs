15376ef27f7b17a1d0af9de76018b5ec1307fe36
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2020-01-22 00:42:56 -0500

    More updates

--- src/commandline.rs
+++ src/commandline.rs
@@ -1,62 +1,96 @@
 use regex::Regex;
+use std::fs::{create_dir_all, File, OpenOptions};
 use std::path::Path;
 use structopt::StructOpt;
 
+/// Parse the user provided regex into its own struct. This will fully support
+/// capture expressions that can later be used to filter out log lines
 fn parse_regex(re: &str) -> Regex {
-    Regex::new(re).unwrap()
+    match Regex::new(re) {
+        Ok(parsed) => parsed,
+        Err(e) => {
+            eprintln!("Failed to initialize Regex. Reason: \n {}", e);
+            std::process::exit(1);
+        }
+    }
 }
 
-fn parse_istream_path(path: &std::ffi::OsStr) -> Box<dyn std::io::Read> {
-    if path.is_empty() || path == "-" {
+/// Return the input stream to use, given a path string as represented by the OS
+/// Notable special cases:
+///     - If path is empty, it returns stdin
+fn istream_from_path(path: &std::ffi::OsStr) -> Box<dyn std::io::Read> {
+    if path.is_empty() || path == "/dev/stdin" {
         Box::new(std::io::stdin())
     } else {
-        Box::new(std::fs::File::open(Path::new(path)).unwrap())
+        Box::new(File::open(Path::new(path)).expect("Failed to open input file"))
     }
 }
 
-fn parse_ostream_path(path: &std::ffi::OsStr) -> Box<dyn std::io::Write> {
-    if path.is_empty() || path == "-" {
-        Box::new(std::io::stdout())
+/// FIXME: This should defer output file creation to the first write
+///
+/// Given an operating system represented path string return a handle to the
+/// output stream that writes to the file pointed to by that path
+/// Notable special cases:
+///     - if path is empty it returns stdout
+fn ostream_from_path(path: &std::ffi::OsStr) -> Box<dyn std::io::Write> {
+    if path.is_empty() || path == "/dev/stdout" {
+        Box::new(std::io::stderr())
     } else {
-        Box::new(std::fs::File::open(Path::new(path)).unwrap())
+        let output_path = Path::new(path);
+        create_dir_all(output_path.parent().unwrap()).expect(&format!(
+            "Unable to create parent dirs for output path: {:?}",
+            path
+        ));
+        Box::new(
+            OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(output_path)
+                .expect("Failed to create, or open output file for truncated write"),
+        )
     }
 }
-
-// pub enum ErrorStream {
-//     Standard(std::io::Stderr),
-//     File(std::fs::File)
-// }
 
 #[derive(StructOpt)]
-#[structopt(name = "logan", about = "Match, parse, and transform your logs")]
+#[structopt(
+    name = "logan",
+    about = "Match, parse, and transform your logs",
+    verbatim_doc_comment
+)]
 pub struct ProgramOptions {
-    /// Run the tool in debug mode. Copious amounts of information will be
-    /// dumped to stderr
-    #[structopt(short)]
-    pub debug: bool,
-
     /// Pattern to search for, syntax is similar to perl regexes
-    #[structopt(
-        short,
-        name="REGEX",
-        parse(from_str=parse_regex)
-    )]
+    #[structopt(short, name="REGEX", parse(from_str=parse_regex))]
     pub pattern: Regex,
 
-    /// Source of input lines, defaults to stdin
-    #[structopt(
-        name = "INFILE",
-        default_value="-",
-        parse(from_os_str=parse_istream_path)
-    )]
-    pub input: Box<dyn std::io::Read>,
+    /// Query to run on the consumed log line
+    #[structopt(name = "QUERY")]
+    pub query: Option<String>,
 
-    /// Sink for results, defaults to stdout
+    /// Path to output file, defaults to stdout.
+    /// {n}Existing files will be truncated before write
     #[structopt(
         short,
         name = "OUTFILE",
-        default_value = "-",
-        parse(from_os_str=parse_ostream_path)
+        default_value = "/dev/stdout",
+        parse(from_os_str=ostream_from_path)
     )]
     pub output: Box<dyn std::io::Write>,
+
+    /// Path to input file, defaults to stdin
+    #[structopt(
+        name = "INFILE",
+        default_value="/dev/stdin",
+        parse(from_os_str=istream_from_path)
+    )]
+    pub input: Box<dyn std::io::Read>,
+}
+
+impl std::fmt::Debug for ProgramOptions {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "ProgramOptions {{ pattern: '{:?}', query: {:?} }}",
+            self.pattern, self.query
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod commandline;
 use commandline::ProgramOptions;
 
+use log;
+use env_logger;
 use regex::Regex;
+
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::string::String;
 use std::vec::Vec;
 use structopt::StructOpt;
 
 /// Return the lines that match our regex
-fn get_matches<R: Read>(pattern: Regex, istream: R) -> Vec<String> {
+fn get_matches(pattern: Regex, istream: impl Read) -> Vec<String> {
     let mut matched_lines: Vec<String> = Vec::new();
     let reader = BufReader::new(istream);
 
@@ -21,8 +24,8 @@ fn get_matches<R: Read>(pattern: Regex, istream: R) -> Vec<String> {
     matched_lines
 }
 
-/// This should probably return Result<usize>
-fn put_matches<W: Write>(lines: Vec<String>, ostream: W) -> Result<(), std::io::Error> {
+/// Write matched lines back to the given output stream
+fn put_matches(lines: Vec<String>, ostream: impl Write) -> Result<(), std::io::Error> {
     let mut writer = BufWriter::new(ostream);
 
     for line in lines {
@@ -33,7 +36,9 @@ fn put_matches<W: Write>(lines: Vec<String>, ostream: W) -> Result<(), std::io::
 }
 
 fn main() {
+    env_logger::init();
     let opts = ProgramOptions::from_args();
+    log::debug!("Using {:?}", opts);
     let matches = get_matches(opts.pattern, opts.input);
-    put_matches(matches, opts.output).expect(&"Error during write!");
+    put_matches(matches, opts.output).expect(&"Error writing results");
 }
